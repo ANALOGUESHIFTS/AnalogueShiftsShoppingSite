74d241330df6cbd0a63c4abdd443fd8ed3d73a81
unknown <pebuka351@gmail.com>
2024-01-06 08:03:17 +0100

    Added social media links

--- app/[locale]/faq/page.js
+++ app/[locale]/faq/page.js
@@ -10,13 +10,7 @@ export const metadata = {
       "Welcome to the FAQ page at Cinnamon19Fashion, your go-to resource for quick answers and helpful insights. We understand you may have questions, and we're here to provide clarity. Explore our frequently asked questions to find information on orders, shipping, returns, and more. If you don't find what you're looking for, feel free to contact our customer support team for personalized assistance. We're dedicated to making your experience with Cinnamon19Fashion as smooth as possible. Discover the answers you need and embark on your fashion journey with confidence.",
     url: "https://cinnamon19fashion.com/en/faq",
     siteName: "Cinnamon19Fashion",
-    images: [
-      {
-        url: "images/ceo.jpg",
-        width: 800,
-        height: 600,
-      },
-    ],
+    images: ["https://cinnamon19fashion.com/images/ceo.jpg"],
     locale: "en_US",
     type: "website",
     alternates: {
--- app/[locale]/profile/page.js
+++ app/[locale]/profile/page.js
@@ -10,13 +10,7 @@ export const metadata = {
       "Welcome to your Profile Page at Cinnamon19Fashion, the personalized hub for your style preferences and account management. Here, you have the power to tailor your fashion journey. Update your profile information, manage your orders, and save your favorite items for later. Your Profile Page is the gateway to a curated shopping experience designed just for you. Stay connected with your order history, track shipments, and discover exclusive offers tailored to your taste. Thank you for entrusting Cinnamon19Fashion with your style, where your profile is the key to a seamless and customized shopping adventure. Enjoy the journey!",
     url: "https://cinnamon19fashion.com/en/profile",
     siteName: "Cinnamon19Fashion",
-    images: [
-      {
-        url: "images/ceo.jpg",
-        width: 800,
-        height: 600,
-      },
-    ],
+    images: ["https://cinnamon19fashion.com/images/ceo.jpg"],
     locale: "en_US",
     type: "website",
     alternates: {
